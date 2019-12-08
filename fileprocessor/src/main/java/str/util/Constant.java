bc82510146e8f4e04da32b301267992a9bd17790
Shekhar Sushant <shekhar.sushant@ad.infosys.com>
2019-12-09 00:30:56 +0100

    constant and Test

---
+++ fileprocessor/src/main/java/str/util/Constant.java
@@ -0,0 +1,12 @@
+package str.util;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Constant {
+
+    public static final String PATH = ".\\resource";
+    public static final String XML_FILE = "xml";
+    public static final String TEXT_FILE = "txt";
+    public static final Charset charset = StandardCharsets.UTF_8;
+}
---
+++ fileprocessor/src/test/java/str/processor/impl/XMLFileProcessorTest.java
@@ -0,0 +1,18 @@
+package str.processor.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XMLFileProcessorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void processXMLFile() {
+    }
+}
